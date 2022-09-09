d14fcedd80e6e78c77cdd066150b833ff39d6b39
ItsBurpee <36286579+ItsBurpee@users.noreply.github.com>
2022-09-08 19:23:38 -0600

    Replaced RingingBloom folder with the modified one.

---
+++ RingingBloom-MHRVoiceChanger/WWiseTypes/ViewModels/NPCKViewModel.cs
@@ -0,0 +1,142 @@
+﻿using MHRVoiceChanger.WWiseTypes.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace MHRVoiceChanger.WWiseTypes.ViewModels
+{
+    /* EDITED for the MHRVoiceChanger
+    General Changes:
+         Removed "MassReplace" and edited the "Export" functions
+    */
+    public class NPCKViewModel  : BaseViewModel
+    {
+        public NPCKHeader npck = null;//shouldn't have to bind directly to this
+
+        public ObservableCollection<Wem> wems 
+        { 
+            get {
+                if(npck == null)
+                {
+                    return new ObservableCollection<Wem>();
+                }
+                else
+                {
+                    return new ObservableCollection<Wem>(npck.WemList);
+                }
+                
+            } 
+            set {
+                npck.WemList = new List<Wem>(value);
+                OnPropertyChanged("wems"); 
+            }
+        }
+        public ObservableCollection<string> languages
+        { 
+            get 
+            { 
+                if(npck == null)
+                {
+                    return new ObservableCollection<string>();
+                }
+                else
+                {
+                    return new ObservableCollection<string>(npck.GetLanguages());
+                }
+            }
+            set => throw new NotImplementedException();
+        }
+
+        public NPCKViewModel()
+        {
+            npck = null;
+        }
+
+        public void SetNPCK(NPCKHeader file)
+        {
+            npck = file;
+            OnPropertyChanged("wems");
+            OnPropertyChanged("languages");
+        }
+
+        public void AddWems(string[] fileNames)
+        {
+            foreach (string fileName in fileNames)
+            {
+                Wem newWem = HelperFunctions.MakeWems(fileName, HelperFunctions.OpenFile(fileName));
+                npck.WemList.Add(newWem);
+            }
+            OnPropertyChanged("wems");
+        }
+
+        public void ReplaceWem(Wem newWem,int index)
+        {
+            newWem.id = npck.WemList[index].id;
+            newWem.languageEnum = npck.WemList[index].languageEnum;
+            npck.WemList[index] = newWem;
+            OnPropertyChanged("wems");
+        }
+        public void DeleteWem(int index)
+        {
+            npck.WemList.RemoveAt(index);
+            OnPropertyChanged("wems");
+        }
+
+        /* EDITED for the MHRVoiceChanger
+            Removed the prompt to save with names
+            It must save with names to work with the Voice Changer components
+        */
+        public void ExportWems(string savePath)
+        {
+            foreach (Wem newWem in npck.WemList)
+            {
+                string name = savePath + "\\" + newWem.name;
+                BinaryWriter bw = new BinaryWriter(new FileStream(name, FileMode.OpenOrCreate));
+                bw.Write(newWem.file);
+                bw.Close();
+            }
+        }
+
+        /* EDITED for the MHRVoiceChanger
+            Rather than putting the header file alongside the main file, it places it in the correct mod directory for a Fluffy mod
+        */
+        public void ExportNPCK(string[] filePaths)
+        {
+            npck.ExportFile(filePaths[0]);
+            npck.ExportHeader(filePaths[1]);
+        }
+
+        public void IDReplace(string[] id2)
+        {
+            for (int i = 0; i < id2.Length; i++)
+            {
+                try
+                {
+                    npck.WemList[i].id = Convert.ToUInt32(id2[i]);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    break;
+                }
+            }
+            OnPropertyChanged("wems");
+        }
+
+        public void ExportLabels(SupportedGames mode, string currentFileName, List<uint> changedIds)
+        {
+            npck.labels.Export(Directory.GetCurrentDirectory() + "/" + mode.ToString() + "/PCK/" + currentFileName + ".lbl", npck.WemList, changedIds);
+        }
+
+        public List<uint> GetWemIds()
+        {
+            List<uint> wemIds = new List<uint>();
+            for (int i = 0; i < npck.WemList.Count; i++)
+            {
+                wemIds.Add(npck.WemList[i].id);
+            }
+            return wemIds;
+        }
+
+    }
+}
---
+++ RingingBloom-MHRVoiceChanger/Windows/NPCKEditor.xaml.cs
@@ -0,0 +1,177 @@
+﻿using MHRVoiceChanger;
+using MHRVoiceChanger.WWiseTypes;
+using MHRVoiceChanger.WWiseTypes.Common;
+using MHRVoiceChanger.WWiseTypes.ViewModels;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace RingingBloom.Windows
+{
+    /// <summary>
+    /// Interaction logic for NPCKEditor.xaml
+    /// </summary>
+
+    /* EDITED for the MHRVoiceChanger
+    A lot of the file has been overhaul to work with the Voice Changer changes
+    At this point, the window assigned to this class is more of a PCK viewer instead of an editor
+    */
+    public partial class NPCKEditor : Window
+    {
+        public SupportedGames mode = SupportedGames.MHRise;
+        public NPCKViewModel viewModel { get; set; }
+        private string ImportPath = null;
+        private string ExportPath = null;
+
+        //Directory paths for "inputWems", "outputWems", and the Fluffy template's main folder and header folder
+        private string tempDirPathIn = Directory.GetCurrentDirectory() + "/inputWems/";
+        private string tempDirPath2 = Directory.GetCurrentDirectory() + "/outputWems/";
+        private string modDirPath = Directory.GetCurrentDirectory() + "/Output Voice Mod/natives/STM/streaming/Sound/Wwise/";
+        private string modNonStreamDirPath = Directory.GetCurrentDirectory() + "/Output Voice Mod/natives/STM/Sound/Wwise/";
+
+        public NPCKEditor(SupportedGames Mode,Options options)
+        {
+            InitializeComponent();
+            mode = Mode;
+            viewModel = new NPCKViewModel();
+            WemView.DataContext = viewModel;
+            if(options.defaultImport != null)
+            {
+                ImportPath = options.defaultImport;
+            }
+            if (options.defaultExport != null)
+            {
+                ExportPath = options.defaultExport;
+            }
+        }
+
+        private void Import_Wems(object sender, RoutedEventArgs e)
+        {
+            if (viewModel.npck == null)
+            {
+                MessageBox.Show("NPCK not loaded.");
+                return;
+            }
+            OpenFileDialog openFile = new OpenFileDialog();
+            if(ImportPath != null)
+            {
+                openFile.InitialDirectory = ImportPath;
+            }
+            openFile.Multiselect = true;
+            openFile.Filter = "WWise Wem files (*.wem)|*.wem";
+            if (openFile.ShowDialog() == true)
+            {
+                viewModel.AddWems(openFile.FileNames);
+            }
+
+
+        }
+
+        /* EDITED for the MHRVoiceChanger
+            The destination for the wems to be exported is predetermined: "inputWems" & "outputWems"
+        */
+        public void Export_Wems()
+        {
+            if (viewModel.npck == null)
+            {
+                MessageBox.Show("NPCK not loaded.");
+                return;
+            }
+            // Setup the directories for "inputWems" and "outputWems"
+            SetupDirectory(tempDirPathIn);
+            SetupDirectory(tempDirPath2);
+            viewModel.ExportWems(tempDirPathIn);
+        }
+
+        /* SUMMARY: Reimport_Wems
+            Reimports the wems from "outputWems" back into the PCK Editor
+        */
+        public void Reimport_Wems()
+        {
+            viewModel.SetNPCK(new NPCKHeader(mode));
+
+            string[] reimportedWems = new string[Directory.GetFiles(tempDirPath2).Length];
+            int arrayIndex = 0;
+            // FOREACH: File in "outputWems", add them to an array for reimporting
+            foreach (string filePath in Directory.GetFiles(tempDirPath2))
+            {
+                reimportedWems[arrayIndex] = filePath;
+                arrayIndex++;
+            }
+
+            viewModel.AddWems(reimportedWems);
+
+            DismantleDirectory(tempDirPathIn);
+            DismantleDirectory(tempDirPath2);
+        }
+
+        /* SUMMARY: SetupDirectory
+            Setups a directory
+            That is, empty the directory for usage if it exist or create it if it doesn't
+
+        PARAMETERS:
+            targetDirectory - The directory that is to be setup
+
+        */
+        private void SetupDirectory(string targetDirectory)
+        {
+            // IF: the directory EXISTS, clear its contents
+            if (Directory.Exists(targetDirectory))
+            {
+                DirectoryInfo tempDI = new DirectoryInfo(targetDirectory);
+                foreach (FileInfo file in tempDI.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+            }
+            // ELSE: the directory DOESN'T EXIST, create it
+            else
+            {
+                Directory.CreateDirectory(targetDirectory);
+            }
+        }
+
+        /* SUMMARY: DismantleDirectory
+        clears then deletes a directory
+
+        PARAMETERS:
+            targetDirectory - The directory that is to be dismantled
+
+        */
+        private void DismantleDirectory(string targetDirectory)
+        {
+            // IF: the directory EXISTS, clear its contents
+            if (Directory.Exists(targetDirectory))
+            {
+                DirectoryInfo tempDI = new DirectoryInfo(targetDirectory);
+                foreach (FileInfo file in tempDI.EnumerateFiles())
+                {
+                    file.Delete();
+                }
+                Directory.Delete(targetDirectory, false);
+            }
+        }
+
+        /* EDITED for the MHRVoiceChanger
+            The destination for the PCK file to be exported is predetermined: A Fluffy ready mod folder template
+        */
+        public void ExportPCK(string targetFileName)
+        {
+            SetupDirectory(modDirPath);
+            SetupDirectory(modNonStreamDirPath);
+            string[] targetFilePaths = new string[] { modDirPath + targetFileName, modNonStreamDirPath + targetFileName };
+            viewModel.ExportNPCK(targetFilePaths);
+
+        }
+
+        /* EDITED for the MHRVoiceChanger
+            The IDList string is predetermined by the file given
+        */
+        public void IDReplace(string targetIDList)
+        {
+            string[] id2 = targetIDList.Split(',');
+            viewModel.IDReplace(id2);
+        }
+
+    }
+}
